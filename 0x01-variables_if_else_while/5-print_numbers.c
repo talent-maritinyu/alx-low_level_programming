cdfc9cd26415578894363b0e9ffad337145173a3
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-17 16:01:12 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Print all base tem numbers from 0
+ *
+ * Retrun: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return(0);
+}
