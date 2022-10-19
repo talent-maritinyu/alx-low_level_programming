5801cb0dba3bbbf83c7cc5458be1ab289b4ed8c4
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-19 04:21:45 -0700

    a function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,9 @@ void jack_bauer(void)
 	int i, j;
 
 	for (i = 0; i <= 24; i++)
+	{
 		for (j = 0; j <= 60; j++)
+		{
 			_putchar((i / 10) + '0');
 	_putchar((i % 10) + '0');
 	_putchar(':');
@@ -17,5 +19,7 @@ void jack_bauer(void)
 	_putchar((j / 10) + '0');
 	_putchar((j % 10) + '0');
 	_putchar('\n');
+		}
+	}
 }
 
