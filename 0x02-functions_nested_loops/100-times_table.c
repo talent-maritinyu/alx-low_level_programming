3537be4064dfac7b23ca0bb9ff05283a9165fffd
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-19 09:15:25 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,7 +41,7 @@ void print_times_table(int n)
 				 _putchar(','); 
 				 _putchar(' ');
 				 _putchar((k / 10) + '0');
-				 _putchar(((k % 10) %10) + '0');
+				 _putchar(((k % 10) % 10) + '0');
 				 _putchar((k % 10) + '0');
 			}
 		}
