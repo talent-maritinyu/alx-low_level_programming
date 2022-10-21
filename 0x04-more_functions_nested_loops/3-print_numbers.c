2dc22e28668533cf0c81e2f9d9b536266d725c3e
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-21 15:15:13 -0700

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0-9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
