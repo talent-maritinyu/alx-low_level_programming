096c5886d12a9e30ec1566095777d6790d9c2be5
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-21 15:08:46 -0700

    function to multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - chech digit to be multiplied
+ * @a: int to be multiplied by b
+ * @b: int to be multiplied by a 
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c =a * b;
+	return (c);
+}
