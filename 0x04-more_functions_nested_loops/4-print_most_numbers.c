d9347fd59589b1382a21d42742ea0d9690bac881
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-21 15:20:25 -0700

    print numerf from 0-9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0-9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
