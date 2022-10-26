3d44923a235ef0be8d6d1ce6bbbe1affbdfebff0
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-25 18:04:30 -0700

    a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 
 	while (count >= 0)
 	{
-		*(dest + count) = *(src + count)
+		*(dest + count) = *(src + count);
 			if (*(src + count) = ='\0')
 				break;
 		count++;
