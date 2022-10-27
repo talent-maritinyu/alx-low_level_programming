9adff0f93c752b583c8557bb675444d617fc19fb
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-27 10:29:15 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,8 +16,10 @@ char *_strcat(char *dest, char *src);
 
 	for (j = 0; src[j] !='\0'; j++)
 		{
-			dest[i] = src[j];
+			dest[i + j] = src[j];
+			if (src[j] == '\0')
+				i = j;
 		}
-	dest[i] = '\0';
+
 	return (dest);
 }
