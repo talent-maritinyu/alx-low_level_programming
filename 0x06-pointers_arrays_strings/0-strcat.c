5917b7f105610316df50eb759484bb2089999133
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-27 10:24:26 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,10 @@ char *_strcat(char *dest, char *src);
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		for (j = 0; src[j] !='\0; j++)
+	{
+	}
+
+	for (j = 0; src[j] !='\0; j++)
 		{
 			dest[i] = src[j];
 		}
