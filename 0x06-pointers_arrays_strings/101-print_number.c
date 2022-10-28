8a7e17d2554aa4052ef76a5a258ef09d1e540fb6
talent-maritinyu <nyandorogift35@gmail.com>
2022-10-27 17:25:06 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers chars
+ * @n: int
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+	
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
